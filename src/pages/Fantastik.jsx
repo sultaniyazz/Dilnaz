b8a8b22f2a6eaac5014057a7c3500eacd1b44fd1
Sultaniyaz <sultaniyazdev@gmail.com>
2025-06-02 10:18:20 +0500

    Update Fantastik.jsx

--- src/pages/Fantastik.jsx
+++ src/pages/Fantastik.jsx
@@ -14,7 +14,7 @@ const Fantastika = () => {
       <div className='lg:py-10 py-8 lg:px-15 px-5'>
         <div>
           <HeaderTop
-            h1={'PangoBooksdagi Fantastik Kitoblar Katalogi'}
+            h1={'Fantastik Kitoblar Katalogi'}
             h2={'Sehrli olamlar, epik sarguzashtlar va g‘ayritabiiy kuchlar bilan to‘la fantastik adabiyotlar to‘plamini kashf eting.'}
             span={'Fantastik janr – bu o‘quvchini kundalik hayot chegarasidan chiqarib, tasavvur olamiga yetaklaydigan adabiy yo‘nalishdir. Bu kitoblar sehrgarlar, ajdaholar, boshqa olamdagi mavjudotlar, sehr-jodu va moʻjizaviy voqealarga boy bo‘ladi. Hikoyalar ko‘pincha yosh qahramonlar atrofida aylanadi, ular o‘z dunyosini qutqarish yoki o‘z ichki kuchini kashf qilish uchun katta sarguzashtga otlanadilar. Har bir sahifada yangi sirlar, hayajonli burilishlar va hissiyotlarga boy voqealar sizni kutadi.'}
             span2={'Fantastik adabiyot o‘quvchiga tasavvurini kengaytirish, yangi g‘oyalar haqida o‘ylash, haqiqat bilan xayolot orasidagi nozik chiziqni anglash imkonini beradi. Bu janr nafaqat yosh kitobxonlarga, balki har qanday yoshdagi o‘quvchilarga mo‘ljallangan. Ko‘plab mashhur fantastik romanlar – masalan, J.K. Rowlingning "Garri Potter", J.R.R. Tolkienning "Uzuklar hukmdori", yoki Suzanne Collinsning "Och o‘yinlar" trilogiyasi – butun dunyo bo‘ylab millionlab o‘quvchilarning qalbini zabt etgan.'}
