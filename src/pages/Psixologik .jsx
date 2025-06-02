0abefb32e6c67ebf034bfc45abae49ae7011e12b
Sultaniyaz <sultaniyazdev@gmail.com>
2025-06-02 10:18:53 +0500

    Update Psixologik .jsx

--- src/pages/Psixologik .jsx
+++ src/pages/Psixologik .jsx
@@ -15,7 +15,7 @@ const Trillerler = () => {
       <div className='lg:py-10 py-8 lg:px-15 px-5'>
         <div>
           <HeaderTop
-            h1={'PangoBooksdagi Psixologik Kitoblar Katalogi'}
+            h1={'Psixologik Kitoblar Katalogi'}
             h2={'Inson ruhiy olamining chuqur sirlarini o‘rganishga yordam beradigan psixologik kitoblarni kashf eting.'}
             span={'Psixologik adabiyot – bu inson qalbining eng nozik, eng murakkab jihatlarini o‘rganishga qaratilgan janrdir. Bu turdagi kitoblarda o‘quvchi ruhiy ziddiyatlar, ichki kurashlar, ong osti jarayonlari va shaxsiy o‘zgarishlarga guvoh bo‘ladi. Qahramonlarning kechinmalari, shubhalari, travmalari va qaror qabul qilish jarayoni markazda turadi. Har bir sahifa o‘quvchini ichki olamga chuqur sho‘ng‘ishga, o‘zini anglashga va o‘z psixologik holatini tahlil qilishga undaydi.'}
             span2={'Psixologik romanlar ko‘pincha keskin dramatik vaziyatlar, og‘ir tanlovlar yoki kutilmagan psixologik burilishlar bilan boyitilgan bo‘ladi. Ular o‘quvchini doimiy ravishda o‘ylantiradi, savollar beradi va javob topishga majbur qiladi. Fyodor Dostoyevskiy, Franz Kafka, Haruki Murakami, Gillian Flynn kabi mualliflar psixologik chuqurlikda yozilgan asarlari orqali dunyo adabiyotida chuqur iz qoldirgan. Bu asarlar sizni faqat hikoya bilan emas, balki o‘zingiz haqingizda ham o‘ylashga undaydi.'}
