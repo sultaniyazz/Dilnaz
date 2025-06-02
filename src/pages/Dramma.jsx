ad65c278480548665f144258a16347189d41432a
Sultaniyaz <sultaniyazdev@gmail.com>
2025-06-02 10:17:50 +0500

    Update Dramma.jsx

--- src/pages/Dramma.jsx
+++ src/pages/Dramma.jsx
@@ -17,7 +17,7 @@ const Detective = () => {
       <div className='lg:py-10 py-8 lg:px-15 px-5'>
         <div>
           <HeaderTop
-            h1={'PangoBooksdagi Drammatik Kitoblar Katalogi'}
+            h1={'Drammatik Kitoblar Katalogi'}
             h2={'Hayotning o‘zi kabi murakkab, og‘riqli va ta’sirli hikoyalarni kashf eting — drammatik adabiyot bilan birga yashang va his eting.'}
             span2={'Drammatik romanlar ko‘pincha oila ichidagi murakkab munosabatlar, jamiyatdagi tengsizlik, insoniy xatolar va kechikkan qarorlar haqida hikoya qiladi. Ular bizni inson sifatida qanday o‘sayotganimiz, qanday tuyg‘ular bilan yashayotganimiz haqida o‘ylashga undaydi. Mualliflar – Toni Morrison, Elena Ferrante, Khaled Hosseini, Colleen Hoover kabi yozuvchilar o‘z asarlari orqali yuraklarni titratadigan, unutilmas drammatik hikoyalar yaratgan.'}
             span3={'Agar siz yurakka ta’sir qiladigan, chuqur maʼnoga ega va hissiy jihatdan boy asarlarni sevuvchi kitobxon bo‘lsangiz — PangoBooksdagi drammatik kitoblar sahifasi aynan siz uchun. Bu yerda siz insoniyatning eng nozik va zaif tomonlarini yorituvchi romanlar, yurakni ezadigan voqealar va umid bilan tugaydigan dramalarni topasiz. Kitobni oching – va boshqa birovning ichki dunyosi bilan yuzma-yuz bo‘ling.'}
